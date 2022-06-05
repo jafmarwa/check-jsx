fc7607bcb789512662f75862c72deecbde672371
jafmarwa <105356245+jafmarwa@users.noreply.github.com>
2022-06-05 13:21:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import './App.css';
+import myProfile from './me.jpg';
+import myVideo from './vid.mp4';
+function App() {
+  
+  return (
+
+      <div classNAme="style">
+  
+
+<h1 className="titleRed">JAAFOURA MARWA</h1>
+
+<br/>
+
+<img src={myProfile} alt="my profile"/>
+
+<br/>
+<img src="/me.jpg" alt='my profile'/>
+
+<div>
+
+  
+  <video src={myVideo} width='320px'height='240px' controls/>
+</div>
+
+    </div>
+  );
+}
+
+
+export default App;
